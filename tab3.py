f5ab4154e88074a814901294d04bcb094b52dc67
Jacek Dzbański <jacek17123@gmail.com>
2026-03-10 18:56:06 +0100

    Add tab3

---
+++ tab3.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from dash import dcc, html
+import plotly.graph_objects as go
+
+def render_tab(df):
+
+    layout = html.Div([html.H1('Kanały sprzedaży',style={'text-align':'center'}),
+                        html.Div([dcc.DatePickerRange(id='sales-range',
+                        start_date=df['tran_date'].min(),
+                        end_date=df['tran_date'].max(),
+                        display_format='YYYY-MM-DD')],style={'width':'100%','text-align':'center'}),
+                        dcc.Graph(id="weekday-storetype"),
+                        html.H4("Profil klientów wg kanału"),
+                        dcc.Graph(id="customers-storetype")
+                        ])
+
+    return layout
